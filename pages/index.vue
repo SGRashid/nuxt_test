4ca63ee22212b015654b7cb20be78e4b9fdd1eaa
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-23 10:34:52 +0500

    develop: Добавил navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container d-flex">
     <div class="m-auto">
-    <h1>Hi</h1>
-      </div>
+      <h1>Hi</h1>
+      <navbar />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Navbar from '../components/navbar'
+export default {
+  components: { Navbar }
+}
 </script>
 
 <style>

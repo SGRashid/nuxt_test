42ab5ca9c8c9bd8c4dee934e479a16f40b5505f6
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-23 10:54:10 +0500

    develop: Отключил eslint)))

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">My site</a>
     <button
       class="navbar-toggler"
       type="button"
@@ -19,35 +19,15 @@
           <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
+          <a class="nav-link" href="#">Avengers</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+          <a class="nav-link" href="#">Lorem Ipsum</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </div>
   </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="container d-flex">
-    <div class="m-auto">
-      <h1>Hi</h1>
+  <div class="w-100">
       <navbar />
     </div>
-  </div>
 </template>
 
 <script>
-import Navbar from '../components/navbar'
+ import Navbar from '../components/navbar'
 export default {
   components: { Navbar }
 }
